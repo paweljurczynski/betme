16e8b96569931477a7ffe31bd8c91c231654677b
paweljurczynski <pawel.jurczynski@live.com>
2018-08-22 19:00:47 +0200

    Move RN to TypeScript

--- App.tsx
+++ App.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default class App extends React.Component {
+export default class App extends React.Component {  
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to startWORKINGon your app2!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
-        <Text>dlaczego mie to nie dziala</Text>
-        <button>Nie wiem czy to zadziala</button>
       </View>
     );
   }
