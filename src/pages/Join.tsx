efe24487116a32ae31e89797577203f2381f892c
paweljurczynski <pawel.jurczynski@live.com>
2018-08-23 17:25:18 +0200

    Create Join View

--- src/pages/Join.tsx
+++ src/pages/Join.tsx
@@ -1,18 +1,34 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { List, ListItem, Button } from 'react-native-elements'
 
 type Props = {
     navigation: any
 }
 
 export class JoinScreen extends React.Component<Props, {}> {
+    ready = () => {
+        alert('Jestem gotowy!');
+    }
+
     render() {
         const { navigation } = this.props;
         const roomCode = navigation.getParam('roomCode');
 
+        const waitingUsers = [
+            { name: 'Maciek' },
+            { name: 'Roman' }
+        ];
+
         return (
             <View style={styles.container}>
-                <Text>Numer pokoju: { roomCode }</Text>
+                <Text style={{ margin: 20 }}>Numer pokoju: {roomCode}</Text>
+                <Text>Oczekiwanie na:</Text>
+                <FlatList
+                    data={waitingUsers}
+                    renderItem={({ item }) => <Text style={{ margin: 10 }}>{item.name}</Text>}
+                />
+                <Button title="Gotowy" onPress={this.ready} />
             </View>
         );
     }
